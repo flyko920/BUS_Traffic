1fb42a3ee168771a9ed80a3664597a4fc705f4ab
flykozhang <83659757@qq.com>
2016-12-19 18:01:58 +0800

    更新代码

--- app/src/main/java/com/xcc/bustraffic/bustraffic/test/TestActivity.java
+++ app/src/main/java/com/xcc/bustraffic/bustraffic/test/TestActivity.java
@@ -10,15 +10,19 @@ import com.xcc.bustraffic.bustraffic.R;
 import com.xcc.bustraffic.bustraffic.api.NetApi;
 import com.xcc.bustraffic.bustraffic.api.callback.BastBeanCallBack;
 import com.xcc.bustraffic.bustraffic.ui.activity.BaseActivity;
+import com.xcc.bustraffic.library.utils.L;
 import com.xcc.bustraffic.library.utils.ZXingUtils;
 
 import butterknife.Bind;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TestActivity extends BaseActivity {
 
-    private static final String URL = "http://192.168.0.101:8080/bus/services/api/weiXinPermission/getToken";
+    private static final String URL_TEST_HTTP = "http://192.168.0.101:8080/bus/services/api/weiXinPermission/getToken";
     private static final String URL_TEST_INMAGE = "http://g.hiphotos.baidu.com/baike/w%3D268%3Bg%3D0/sign=ad7e701b7ef0f736d8fe4b07326ed424/3801213fb80e7bec52fd4585292eb9389b506baa.jpg";
-    
+
     @Bind(R.id.button)
     Button button;
     @Bind(R.id.activity_test)
@@ -38,7 +42,6 @@ public class TestActivity extends BaseActivity {
 
     @Override
     public void initData() {
-        run5();
         button.setText("测试哈哈哈哈啊哈");
     }
 
@@ -47,9 +50,10 @@ public class TestActivity extends BaseActivity {
 
         switch (v.getId()) {
             case R.id.button:
+                run5();
                 button.setText("点击变化了哦····");
-//                imageView.setImageBitmap(ZXingUtils.createQRImage(URL,imageView.getWidth(),imageView.getHeight()));//在imageView中显示二维码图片
-                Picasso.with(mContext).load(URL_TEST_INMAGE).into(imageView);
+//                imageView.setImageBitmap(ZXingUtils.createQRImage(URL_TEST_HTTP,imageView.getWidth(),imageView.getHeight()));//在imageView中显示二维码图片
+                Picasso.with(mContext).load(URL_TEST_INMAGE).placeholder(R.mipmap.ic_launcher).error(R.mipmap.ic_launcher).into(imageView);
                 break;
             default:
                 break;
