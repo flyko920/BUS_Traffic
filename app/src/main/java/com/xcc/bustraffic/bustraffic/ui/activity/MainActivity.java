d34ed8223e3f74e829a2e6859fa946578f527900
flykozhang <83659757@qq.com>
2016-12-22 12:34:11 +0800

    完成  车机页面的布局

---
+++ app/src/main/java/com/xcc/bustraffic/bustraffic/ui/activity/MainActivity.java
@@ -0,0 +1,76 @@
+package com.xcc.bustraffic.bustraffic.ui.activity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.umeng.analytics.MobclickAgent;
+import com.xcc.bustraffic.bustraffic.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity {
+
+    @Bind(R.id.imageView3)
+    ImageView imageView3;
+    @Bind(R.id.button3)
+    Button button3;
+
+    @Override
+    public int getLayoutId() {
+        MobclickAgent.openActivityDurationTrack(false); //禁止默认的页面统计方式，这样将不会再自动统计Activity
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initListener() {
+        imageView3.setOnClickListener(this);
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void processClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageView3:
+                dialog1();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void dialog1() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);  //先得到构造器
+        builder.setMessage("你的小车车还有10天到期，后期后将无法享受上网服务，请及时充值哦！"); //设置内容
+        builder.setPositiveButton("充值", new DialogInterface.OnClickListener() { //设置确定按钮
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss(); //关闭dialog
+                Toast.makeText(MainActivity.this, "充值" + which, Toast.LENGTH_SHORT).show();
+                updataRechargeUi();
+            }
+        });
+        builder.setNegativeButton("暂不需要", new DialogInterface.OnClickListener() { //设置取消按钮
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this, "暂不需要" + which, Toast.LENGTH_SHORT).show();
+            }
+        });
+        //参数都设置完成了，创建并显示出来
+        builder.create().show();
+    }
+
+    private void updataRechargeUi() {
+        button3.setText("已充值");
+    }
+}
---
+++ app/src/main/java/com/xcc/bustraffic/bustraffic/ui/activity/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.xcc.bustraffic.bustraffic.ui.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.view.View;
+
+import com.xcc.bustraffic.bustraffic.R;
+
+public class SplashActivity extends BaseActivity {
+
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void initData() {
+    }
+
+    @Override
+    public void processClick(View v) {
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // 延迟跳转到主界面
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
